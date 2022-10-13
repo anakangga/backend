e2864313162080c32addcd0f800eb6636c411145
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2022-10-13 23:37:37 +0700

    feat: add site id to django settings

--- backend/settings.py
+++ backend/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     "main",
 ]
 
+SITE_ID = int(os.getenv("SITE_ID", 1))
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
