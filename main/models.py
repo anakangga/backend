807bc44b0f1009fba15bb4b6f85f32bcb904cc24
Puspita Sari Matovanni <vaynnipuspita29@gmail.com>
2022-10-20 15:45:48 +0700

    add models for video

--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from user.models import Pengguna
 
 # Create your models here.
+class VideoCustomer(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(Pengguna, on_delete=models.CASCADE, related_name='videos')
+    video = models.FileField(upload_to='videos/')
+
+    def __str__(self):
+        return self.title
+    
+class InterpreterVideo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(Pengguna, on_delete=models.CASCADE, related_name='interpreter_videos')
+    video = models.FileField(upload_to='videos/')
+    video_customer = models.ForeignKey(VideoCustomer, on_delete=models.CASCADE, related_name='interpreter_videos')
+
+    def __str__(self):
+        return self.title
